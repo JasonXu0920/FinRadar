0d3d4c9989114a268b46825aa11f837fab88ea39
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2023-12-06 20:45:04 +1100

    Built models and db migrations

---
+++ api/Models/Comment.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace api.Models
+{
+    public class Comment
+    {   
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn {get; set;} = DateTime.Now;
+        public int? StockId {get; set;}
+        // Navigation
+        public Stock? Stock {get; set;}
+
+    }
+}
