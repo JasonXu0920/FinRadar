4b79d5b3b7db7cbc6c6cf6ba201a87bafc8fd04a
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2024-01-31 08:38:20 +1100

    Auth Services

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import { ToastContainer } from 'react-toastify';
 import './App.css';
+import "react-toastify/dist/ReactToastify.css";
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
 import {Outlet} from 'react-router';
@@ -8,6 +10,7 @@ function App() {
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
       <Footer />
     </>
   );
