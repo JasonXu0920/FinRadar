ee42d1f27861006154d45830fa15044cc6dbce26
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2024-02-03 09:14:04 +1100

    Log In

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b79d5b3b7db7cbc6c6cf6ba201a87bafc8fd04a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d87636bf9f17ec107f654d576deea818f038bed")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
