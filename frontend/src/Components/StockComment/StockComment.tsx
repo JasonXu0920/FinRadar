6426c4e9a9d48f3fe702624de88b3933d98cf664
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2024-02-24 11:14:09 +1100

    comment list

--- frontend/src/Components/StockComment/StockComment.tsx
+++ frontend/src/Components/StockComment/StockComment.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockCommentForm from "./StockCommentForm/StockCommentForm";
-import { commentPostAPT } from "../../Services/CommentService";
+import { commentGetAPI, commentPostAPI } from "../../Services/CommentService";
 import { toast } from "react-toastify";
+import { CommentGet } from "../../Models/Comment";
+import Spinner from "../Spinner/Spinner";
+import StockCommentList from "../StockCommentList/StockCommentList";
 
 type Props = {
   stockSymbol: string;
@@ -12,11 +15,19 @@ type CommentFormInputs = {
 };
 
 const StockComment = ({ stockSymbol }: Props) => {
+  const [comments, setComment] = useState<CommentGet[] | null>(null);
+  const [loading, setLoading] = useState<boolean>();
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
   const handleComment = (e: CommentFormInputs) => {
-    commentPostAPT(e.title, e.content, stockSymbol)
+    commentPostAPI(e.title, e.content, stockSymbol)
       .then((res) => {
         if (res) {
           toast.success("Comment created successfully!");
+          getComments();
         }
       })
       .catch((e) => {
@@ -24,7 +35,21 @@ const StockComment = ({ stockSymbol }: Props) => {
       });
   };
 
-  return <StockCommentForm symbol={stockSymbol} handleComment={handleComment} />;
+  const getComments = () => {
+    setLoading(true);
+    commentGetAPI(stockSymbol).then((res) => {
+      setLoading(false);
+      setComment(res?.data!);
+    });
+  };
+
+  return (
+    <div className="flex flex-col">
+      {loading ? <Spinner/> : <StockCommentList comments={comments!}  />}
+      <StockCommentForm symbol={stockSymbol} handleComment={handleComment} />
+
+    </div>
+  );
 };
 
 export default StockComment;
